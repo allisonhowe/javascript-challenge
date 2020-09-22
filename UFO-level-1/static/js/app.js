967e8beab1385a54ad3a4dac58b592df5d30f7ed
Allison Howe <allisonhowe77@gmail.com>
2020-09-21 20:54:46 -0400

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,19 +2,15 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-var tableBodyElement = document.getElementById("ufo-table").getElementsByTagName("tbody")[0];
+var tbody = d3.select("tbody");
 
 var i;
 for (i = 0; i < tableData.length; i++) {
-    var newRow = tableBodyElement.insertRow();
-    newRow.innerHTML = `<tr>
-        <td>${tableData[i].datetime}</td>
-        <td>${tableData[i].city}</td>
-        <td>${tableData[i].state}</td>
-        <td>${tableData[i].country}</td>
-        <td>${tableData[i].shape}</td>
-        <td>${tableData[i].durationMinutes}</td>
-        <td>${tableData[i].comments}</td>
-    </tr>`;
+    var newRow = tbody.append("tr");
+    Object.entries(tableData[i]).forEach(([key, value]) => {
+        var cell = newRow.append("td");
+        cell.text(value);
+    });
+
 }
 
